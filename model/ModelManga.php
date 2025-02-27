fd640f13126a881d1d7cd4ae4eba7da5c17223ac
Jackmaa <valentingillot@gmail.com>
2025-02-27 16:21:09 +0100

    🎉 ✨ Refactor manga retrieval to filter by category and return all relevant entries✨🎉

--- model/ModelManga.php
+++ model/ModelManga.php
@@ -1,9 +1,20 @@
 <?php
 class ModelManga extends Model {
-    public function getMangaList() {
-        $req  = $this->getDb()->query('SELECT `id_manga`, `name`, `description`, `published_date`, `thumbnail` FROM `manga`');
-        $data = $req->fetch(PDO::FETCH_ASSOC);
-        var_dump($data);
-        return new Manga($data);
+    public function getMangaListByCat() {
+        $req    = $this->getDb()->query('SELECT * FROM category, manga WHERE category.name = "shonen" AND manga.thumbnail != ""');
+        $data   = $req->fetchAll(PDO::FETCH_ASSOC);
+        $mangas = [];
+        foreach ($data as $manga) {
+            $mangas[] = new Manga($manga);
+        }
+        return $mangas;
     }
 }
+
+/*foreach ($mangas as $manga):
+    <div class="manga">
+        <a href="#"><img src="<?php echo $manga->getThumbnail() ?>" alt="<?php echo $manga->getName() ?>"><?php echo $manga->getName() ?></a>
+        <span class="heart"><img src="public\asset\img\heart.svg" alt="Heart"></span>
+        <figure><img src="public\asset\img\star.svg" alt="">(300)</figure>
+    </div>
+endforeach; ?>*/
