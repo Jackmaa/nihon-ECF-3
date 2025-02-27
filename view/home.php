e27ace23297e7cf8c8245a193fc0c61c25ac794d
Jackmaa <valentingillot@gmail.com>
2025-02-27 13:36:16 +0100

    🗑️ Remove deprecated home view file to streamline the codebase 🗑️

--- view/home.php
+++
@@ -1,7 +0,0 @@
-
-<div class="manga">
-    <img src="./asset/public/img/<?php echo $mangas->getThumbnail() ?>.jpg" alt="<?php echo $mangas->getName() ?>">
-    <h2><?php echo $mangas->getName() ?></h2>
-    <p><?php echo $mangas->getDescription() ?></p>
-    <p>Published on: &nbsp;<?php echo $mangas->getPublished_date()->format('d-m-Y') ?></p>
-</div>
