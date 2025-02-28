9147e29a4b55de0f722de787aa4054d42ba32e55
Jackmaa <valentingillot@gmail.com>
2025-02-28 10:32:42 +0100

    :art:Improve code formatting and structure in login.php:art:

---
+++ view/template_part/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public\asset\css\styles.css">
+    <meta name="description" content="<?=$meta_description?>">
+    <title><?=$title?></title>
+</head>
+
+<body>
+    <header class="p-all-2">
+        <section class="menu-mobile">
+            <figure class="menu-burger menuToggle">
+                <img src="public/asset/img/menu_bar.svg" alt="bar menu">
+            </figure>
+            <figure><img src="public\asset\img\logo.svg" alt="Logo"></figure>
+            <figure><img src="public\asset\img\search.svg" alt="search bar"></figure>
+            <div class="burger-menu menuBurger">
+                <div class="menu-header">
+                    <img src="public\asset\img\logo.svg" alt="Menu Logo" class="menu-logo">
+                    <span class="close-menu">×</span>
+                </div>
+                <ul>
+                    <li><a href="#">Connexion</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">My Books</a></li>
+                    <li><a href="#">Favorites</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="menu-desktop">
+            <div>
+                <figure><a href="#"><img src="public\asset\img\logo.svg" alt="Logo"></a></figure>
+                <figure><a href="#"><img src="public\asset\img\search.svg" alt="loup"></a></figure>
+            </div>
+        </section>
+
+    </header>
