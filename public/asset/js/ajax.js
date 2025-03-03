9686d90e7f5cccbc7da4391978efd5dea3e59acc
Jackmaa <valentingillot@gmail.com>
2025-03-03 13:06:17 +0100

    :zap: Implement author search feature with AJAX and update form structure:zap:
    goq
    
    wq

---
+++ public/asset/js/ajax.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function () {
+  let createForm = document.getElementById("create-form");
+  let createAuthorInput = document.getElementById("author");
+  let response = document.getElementById("response");
+
+  createAuthorInput.addEventListener("input", function () {
+    response.innerHTML = "";
+    let formData = new FormData(createForm);
+    fetch("/authorAJAX", {
+      method: "POST",
+      body: formData,
+    })
+      .then((datas) => datas.json())
+      .then((datas) => {
+        const ulElement = document.createElement("ul");
+        for (author of datas) {
+          let suggestionList = document.createElement("li");
+          suggestionList.innerHTML = author.name;
+          ulElement.appendChild(suggestionList);
+        }
+        response.appendChild(ulElement);
+      });
+  });
+});
