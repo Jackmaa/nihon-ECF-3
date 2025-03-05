21afaab3e226a9c918bf60a23ebac5c22ab3fad3
Jackmaa <valentingillot@gmail.com>
2025-03-05 17:28:57 +0100

    ✨ 🚑 Add categories to MangaDTO and update related methods ✨ 🚑

--- entity/MangaDTO.php
+++ entity/MangaDTO.php
@@ -2,9 +2,11 @@
 class MangaDTO {
     public readonly Manga $manga;
     public readonly string $author;
+    public readonly string $categories;
 
-    public function __construct(Manga $manga, string $author) {
+    public function __construct(Manga $manga, string $author, string $categories) {
         $this->manga = $manga;
         $this->author = $author;
+        $this->categories = $categories;
     }   
 }
