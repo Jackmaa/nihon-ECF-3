f7bf6f815db8646ec065b1532183d33c16ee3d36
Jackmaa <valentingillot@gmail.com>
2025-03-12 12:20:25 +0100

    ✨ Refactor Borrow class: add id_volume property and update getter/setter methods

--- entity/Borrow.php
+++ entity/Borrow.php
@@ -3,9 +3,9 @@ class Borrow {
     private int $id_borrow;
     private int $id_manga;
     private int $id_user;
+    private int $id_volume;
     private DateTime $borrow_date;
-    private bool $user_premium;
-    private string $return_date;
+    private DateTime $return_date;
 
     public function __construct(array $datas) {
         $this->hydrate($datas);
@@ -22,31 +22,29 @@ class Borrow {
     }
 
 //GETTERS
-    public function getId() {
+    public function getId_borrow() {
         return $this->id_borrow;
     }
-
+    public function getId_user() {
+        return $this->id_user;
+    }
     public function getId_manga() {
         return $this->id_manga;
     }
 
-    public function getId_user() {
-        return $this->id_user;
+    public function getId_volume() {
+        return $this->id_volume;
     }
 
     public function getBorrow_date() {
         return $this->borrow_date;
     }
 
-    public function getUser_premium() {
-        return $this->user_premium;
-    }
-
     public function getReturn_date() {
         return $this->return_date;
     }
 //SETTERS
-    public function setId(int $id_borrow) {
+    public function setId_borrow(int $id_borrow) {
         $this->id_borrow = $id_borrow;
     }
 
@@ -58,15 +56,15 @@ class Borrow {
         $this->id_user = $id_user;
     }
 
+    public function setId_volume(int $id_volume) {
+        $this->id_volume = $id_volume;
+    }
+
     public function setBorrow_date(string $borrow_date) {
         $this->borrow_date = new DateTime($borrow_date);
     }
 
-    public function setUser_premium(bool $user_premium) {
-        $this->user_premium = $user_premium;
-    }
-
     public function setReturn_date($return_date) {
-        $this->return_date = $return_date;
+        $this->return_date = new DateTime($return_date);
     }
 }
