14c345a98dae46fd3001e5a279d9c62cf0ebf214
SamyMechiche <mechiche.samysm@gmail.com>
2025-03-17 14:30:06 +0100

    fixed small changes, text syntax error and added user's profile pic for reviews

--- view/currentStorie.php
+++ view/currentStorie.php
@@ -21,7 +21,7 @@
                 <div class="current-info">
                     <h4><?php echo $manga->manga->manga->getName()?></h4>
                     <div>
-                        <a href="#">catégory: <span><?php echo $manga->manga->categories?></span></a>
+                        <a href="#">Category: <span><?php echo $manga->manga->categories?></span></a>
                         <a href="#">Author: <span><?php echo $manga->manga->author?></span></a>
                         <a href="#">Editor: <span><?php echo $manga->manga->editor?></span></a>
                     </div>
