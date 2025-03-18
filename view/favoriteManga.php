e38795cb704d00c8661788e7e24b5504bc14ff88
SamyMechiche <mechiche.samysm@gmail.com>
2025-03-18 15:13:12 +0100

    added sort function to favorite pages

--- view/favoriteManga.php
+++ view/favoriteManga.php
@@ -1,7 +1,7 @@
 <?php
 $title            = 'Nihon | Favorite Manga';
 $meta_description = 'Your favorite manga';
-$scripts          = ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js", "public\asset\js\base.js", "public\asset\js\home.js", "public/asset/js/header.js", "public/asset/js/like.js", "public\asset\js\darkmode.js"];
+$scripts          = ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js", "public\asset\js\base.js", "public\asset\js\home.js", "public/asset/js/header.js", "public/asset/js/like.js", "public\asset\js\darkmode.js", "public\asset/js/favorite.js"];
 ob_start();
 ?>
 <main>
@@ -13,33 +13,47 @@ ob_start();
         <img src="<?php echo BASE_URL ?>public\asset\img\current-active.svg" alt="">
     </figure>
     <section>
-    <?php foreach ($favs as $fav): ?>
-        <div class="card-current">
-            <div class="current-storie">
-                <figure><img src="<?php echo $fav->manga->getThumbnail() ?>" alt="<?php echo $fav->manga->getName()?>"></figure>
-                <div class="current-info">
-                    <h4><?php echo $fav->manga->getName()?></h4>
-                    <div>
-                        <a href="#">Category: <span><?php echo $fav->categories?></span></a>
-                        <a href="#">Author: <span><?php echo $fav->author?></span></a>
-                        <a href="#">Editor: <span><?php echo $fav->editor?></span></a>
+    <label for="sort-options">Sort by :</label>
+    <select id="sort-options">
+        <option value="default">Default</option>
+        <option value="name-asc">Name (A-Z)</option>
+        <option value="name-desc">Name (Z-A)</option>
+        <option value="date-recent">Date (from latest to oldest)</option>
+        <option value="date-old">Date (from oldest to latest)</option>
+    </select>
+
+    <div id="fav-container">
+        <?php foreach ($favs as $fav): ?>
+            <div class="card-current" 
+                 data-name="<?php echo strtolower($fav->manga->getName()); ?>" 
+                 data-date="<?php echo $fav->manga->getPublished_date()->format('Y-m-d'); ?>">
+                <div class="current-storie">
+                    <figure><img src="<?php echo $fav->manga->getThumbnail() ?>" alt="<?php echo $fav->manga->getName()?>"></figure>
+                    <div class="current-info">
+                        <h4><?php echo $fav->manga->getName()?></h4>
+                        <div>
+                            <a href="#">Category: <span><?php echo $fav->categories?></span></a>
+                            <a href="#">Author: <span><?php echo $fav->author?></span></a>
+                            <a href="#">Editor: <span><?php echo $fav->editor?></span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="chevron-container">
+                    <div class="chevron-current">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 9L12 15L18 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </div>
+                    <div class="chevron-content">
+                        <p>coucou</p>
+                        <p>Tu veux voir mon git ?</p>
                     </div>
                 </div>
             </div>
-            <div class="chevron-container">
-                <div class="chevron-current">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 9L12 15L18 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </div>
-                <div class="chevron-content">
-                    <p>coucou</p>
-                    <p>Tu veux voir mon git ?</p>
-                </div>
-            </div>
-        </div>
         <?php endforeach; ?>
-    </section>
+    </div>
+</section>
+
 
     <div class="separation">AJOUTER UNE SÉPARATION ICI SVP</div><br><br>
 
