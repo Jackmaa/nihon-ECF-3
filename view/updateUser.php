e443d02ddaa39a42e8d31be9ab0fe2bb69578656
Jackmaa <valentingillot@gmail.com>
2025-03-24 07:00:58 +0100

    Update user profile form to display user's name and improve input formatting

--- view/updateUser.php
+++ view/updateUser.php
@@ -11,18 +11,18 @@
             <h2>Edit Profile</h2>
         </div>
         <figure>
-            <img id="profile-picture" class="profile-picture" src="<?php echo BASE_URL . $_SESSION['profile_pic']?>" alt="profile picture">
+            <img id="profile-picture" class="profile-picture" src="<?php echo BASE_URL . $_SESSION['profile_pic'] ?>" alt="profile picture">
             <button type="button" class="change-picture" id="changePictureBtn">Change picture</button>
             <input type="file" name="profile_pic" id="fileInput" accept="image/*" style="display: none;">
-            <p><?php echo htmlspecialchars($_SESSION['username'])?></p>
+            <p><?php echo htmlspecialchars($_SESSION['name']) ?></p>
         </figure>
     </div>
 </section>
 
 <section class="modify-profile">
-    <form action="<?php echo BASE_URL?>updateProfile.php" method="POST" enctype="multipart/form-data">
-        <input type="text" name="username" placeholder="Username" value="<?php echo htmlspecialchars($_SESSION['username'])?>">
-        <input type="email" name="email" placeholder="Email" value="<?php echo htmlspecialchars($_SESSION['email'])?>">
+    <form action="<?php echo BASE_URL ?>updateProfile.php" method="POST" enctype="multipart/form-data">
+        <input type="text" name="username" placeholder="Username" value="<?php echo htmlspecialchars($_SESSION['name']) ?>">
+        <input type="email" name="email" placeholder="Email" value="<?php echo htmlspecialchars($_SESSION['email']) ?>">
         <input type="password" name="password" placeholder="New Password">
         <input type="password" name="password_verify" placeholder="Confirm New Password">
 
